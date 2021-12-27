bf58b96b91260a75d07e0635123e5f8252207998
JeremiasB23 <jerebutassi@gmail.com>
2021-12-27 19:01:53 -0300

    Testring

---
+++ script.js
@@ -0,0 +1,14 @@
+const text = document.querySelector('.title');
+const changeColor = document.querySelector(".changeColor");
+
+text.style.color = "blue";
+
+
+let x = 0;
+if(x == 0){
+    text.style.color = "blue";
+}else{
+    text.style.color = "green";
+}
+
+//https://www.youtube.com/watch?v=wiozYyXQEVk

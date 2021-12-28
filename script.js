9607774285c01b00d2e55710d1775d0312b07633
JeremiasB23 <jerebutassi@gmail.com>
2021-12-28 06:50:11 -0300

    Just some testing

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 const text = document.querySelector('.title');
+const text2 = document.querySelector('.someText')
 const changeColor = document.querySelector(".changeColor");
 
-text.style.color = "blue";
 
+//text.style.color = "blue";
+text2.classList.add("changeStyle");//!We can add classes
 
-let x = 0;
-if(x == 0){
-    text.style.color = "blue";
-}else{
-    text.style.color = "green";
+changeColor.addEventListener("click", function(){
+
+   //text.classList.add("changeStyle");
+   //text.classList.transform
+   //text.classList.remove
+   text.classList.toggle("changeStyle");
+});
+
+//*All the previous code we are just taking all elements as one, but we can modify each individual
+
+const userList = document.querySelectorAll('.name-list li');
+
+for(user of userList){
+    user.addEventListener("click", function(){
+       this.style.color = "green";
+    });
 }
 
-//https://www.youtube.com/watch?v=wiozYyXQEVk
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
+const userList2 = document.querySelector(".name-list2");
+
+addListBtn.addEventListener("click", function(){
+
+   const newLi = document.createElement("LI");
+   //!El LI lo podemos cambiar por varias cosas ejemplo un h1, a progress var,butoon y cuando apretemos nos lo va a generar
+   const liContent = document.createTextNode(listInput.value);
+   //!I think this is used to create a content who is inside the CreatedElement
+   //!Con element.value podemos ver el valor introducido por el usuario o algo asi
+
+   newLi.appendChild(liContent);
+   userList2.appendChild(newLi);
+});
